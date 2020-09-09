416f3ae64360fe204667c444a9850da7bce10d0c
Iqbal Singh <iqbalsingh673@gmail.com>
2020-09-10 05:01:44 +0530

    updating upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=iqbalsingh673/udacityproj4-ml-microservices-k8s
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag udacityproj4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
