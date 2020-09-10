ee9151a87bb26fbaf958d3f13d8486064a158ba4
Iqbal Singh <iqbalsingh673@gmail.com>
2020-09-10 05:35:02 +0530

    updating run_kubernetes.sh and kubernetes_out.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=iqbalsingh673/udacityproj4-ml-microservices-k8s
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproj4-ml --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployments/udacityproj4-ml 8000:80
